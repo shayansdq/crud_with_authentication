6f74962c275233be9919a70dff45f63b0a5781f2
Shayan <shayansdq20@gmail.com>
2023-08-21 17:34:16 +0330

    add urls

---
+++ authenticate/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import UserRegistrationView, UserLoginView
+
+app_name = 'authenticate'
+
+urlpatterns = [
+    path('register/', UserRegistrationView.as_view(), name='register'),
+    path('login/', UserLoginView.as_view(), name='login'),
+]
